e33f38449c6b84b9ac1e84a9386ae5ecba83f634
rodgersochieng <sirrodgers21@gmail.com>
2022-11-26 18:08:57 +0300

    completed lab

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  // const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
